b6e0b49c1f6f97d6ce0f210307824810d9c58ce0
matthew-eiley <matthew.eiley@mail.mcgill.ca>
2025-09-02 22:15:14 -0400

    added better documentation and reads a specific sheet now

--- get_staff.py
+++ get_staff.py
@@ -1,9 +1,25 @@
 import pandas as pd
+from random import randint
 
-STAFF_EXCEL_PATH = "staff.xlsx"
+"""
+RANDINT RANGES
+- BEER DELIVERY : 1-5
+- SET-UP : 6-16
+- 5A6 : 17-30
+- 6A7 : 31-44
+- 7A8 : 45-58
+- CLEAN-UP : 59-70
+"""
+
+EXCEL_PATH = "staff.xlsx"
+STAFF_SHEET_NAME = "STAFF"
 
 def read_staff_excel(file_path):
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(io=file_path, sheet_name=STAFF_SHEET_NAME)
     return df
 
-print(read_staff_excel(STAFF_EXCEL_PATH))
+
+def main():
+    df = read_staff_excel(EXCEL_PATH)
+    print(df)
+
